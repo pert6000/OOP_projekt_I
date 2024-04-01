073c6a760da16454b98cf4f689c6a8668b71ce47
jaagups <jaagupsass@gmail.com>
2024-04-01 22:14:03 +0300

    Failist küsimine ning osad arvutamised lisasin

--- projekt/src/Main.java
+++ projekt/src/Main.java
@@ -1,31 +1,75 @@
-import java.sql.SQLOutput;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        failistLugemine();
+    }
+    public static void failistLugemine() {
+        System.out.println("Sisesta failitee: ");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String failitee = scanner.nextLine();
+        scanner.close();
 
-        double[] test = {2.4, 5, 6, 1, 19}; // massiiv meetodite testimiseks
-        max s = new max(test);
-        min m = new min(test);
-        vahemik v = new vahemik(test);
-        summa sum = new summa(test);
-        kogus k = new kogus(test);
+        List<Double> arvud = new ArrayList<>(); // List kuhu arvud loetakse failist
 
-        kurtosis kurt = new kurtosis(test);
-        mediaan med = new mediaan(test);
-        asümmeetrijakordaja asüm = new asümmeetrijakordaja(test);
-        standardviga stanV = new standardviga(test);
+        try {
+            File fail = new File(failitee);
+            Scanner failiScanner = new Scanner(fail);
 
-        System.out.println(stanV.getVäärtus());
-        System.out.println(asüm.getVäärtus());
-        System.out.println(s.getVäärtus());
-        System.out.println(m.getVäärtus());
-        System.out.println(v.getVäärtus());
-        System.out.println(sum.getVäärtus());
-        System.out.println(k.getVäärtus());
+            // Loeb arvud failist ja lisab listi
+            while (failiScanner.hasNextLine()) {
+                String rida = failiScanner.nextLine();
+                try {
+                    double arv = Double.parseDouble(rida); // teeb rea double väärtuseks
+                    arvud.add(arv); // Lisab arvu listi
+                } catch (NumberFormatException e) {
+                    System.out.println("Ignoreerin rida " + rida);
+                }
+            }
+            failiScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ei leia faili");
+        }
 
-        System.out.println(kurt.getVäärtus());
+        // Arvutame failist saadud arvudega
+        if (!arvud.isEmpty()) {
+            // Muudab listi massiiviks
+            double[] arvudeMassiiv = new double[arvud.size()];
+            for (int i = 0; i < arvud.size(); i++) {
+                arvudeMassiiv[i] = arvud.get(i);
+            }
 
-        System.out.println(med.getVäärtus());
+            // Teeb arvutused massiivi abil
+            max s = new max(arvudeMassiiv);
+            min m = new min(arvudeMassiiv);
+            vahemik v = new vahemik(arvudeMassiiv);
+            summa sum = new summa(arvudeMassiiv);
+            kogus k = new kogus(arvudeMassiiv);
+            kurtosis kurt = new kurtosis(arvudeMassiiv);
+            mediaan med = new mediaan(arvudeMassiiv);
+            asümmeetrijakordaja asüm = new asümmeetrijakordaja(arvudeMassiiv);
+            standardviga stanV = new standardviga(arvudeMassiiv);
+            mood mo = new mood(arvudeMassiiv);
+            dispersioon dis = new dispersioon(arvudeMassiiv);
 
+            // Prindib tulemused
+            System.out.println("Dispersioon: " + dis.getVäärtus());
+            System.out.println("Mood: " + mo.getVäärtus());
+            System.out.println("Standardviga: " + stanV.getVäärtus());
+            System.out.println("Asümmeetrijakordaja: " + asüm.getVäärtus());
+            System.out.println("Max: " + s.getVäärtus());
+            System.out.println("Min: " + m.getVäärtus());
+            System.out.println("Vahemik: " + v.getVäärtus());
+            System.out.println("Summa: " + sum.getVäärtus());
+            System.out.println("Kogus: " + k.getVäärtus());
+            System.out.println("Kurtosis: " + kurt.getVäärtus());
+            System.out.println("Mediaan: " + med.getVäärtus());
+        } else {
+            System.out.println("Failist ei leitud sobivaid arve arvutamiseks.");
+        }
     }
 }
---
+++ projekt/src/mood.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class mood extends statistilineNäitaja{
+    private double väärtus;
+    public mood(double[] a) {
+        super(a);
+    }
+    public double arvuta(double[] a ){
+        Map<Integer, Integer> lugeja = new HashMap<>();
+
+        for (double number : a){
+            int võti  = (int) number; // muudab double int-iks
+            lugeja.put(võti, lugeja.getOrDefault(võti, 0) + 1);
+        }
+        int mood = 0;
+        int suurim = 0;
+        for (Map.Entry<Integer, Integer> el : lugeja.entrySet()){
+            int mitu = el.getValue();
+            if (mitu > suurim){
+                suurim = mitu;
+                mood = el.getKey();
+            }
+        }
+        väärtus = mood;
+        return mood;
+    }
+    public String selgita() {
+        return "Arv mida failis kõige rohkem esineb on " + väärtus;
+    }
+}
